6148bb1e0cdc72c9112e582f30c6f23f4f9265fb
Simeon Atanasov <S.Atanasov@student.tudelft.nl>
2022-12-11 18:35:19 +0100

    Remove DefaultController
    It was used as reference during development and is no longer necessary.

--- voting-microservice/src/test/java/nl/tudelft/sem/template/voting/integration/VotingTest.java
+++ voting-microservice/src/test/java/nl/tudelft/sem/template/voting/integration/VotingTest.java
@@ -25,7 +25,7 @@ import org.springframework.test.web.servlet.ResultActions;
 @ActiveProfiles({"test", "mockTokenVerifier", "mockAuthenticationManager"})
 //@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
-public class ExampleTest {
+public class VotingTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -36,28 +36,4 @@ public class ExampleTest {
     @Autowired
     private transient AuthManager mockAuthenticationManager;
 
-
-    @Test
-    public void helloWorld() throws Exception {
-        // Arrange
-        // Notice how some custom parts of authorisation need to be mocked.
-        // Otherwise, the integration test would never be able to authorise as the authorisation server is offline.
-        when(mockAuthenticationManager.getNetId()).thenReturn("ExampleUser");
-        when(mockJwtTokenVerifier.validateToken(anyString())).thenReturn(true);
-        when(mockJwtTokenVerifier.getNetIdFromToken(anyString())).thenReturn("ExampleUser");
-
-        // Act
-        // Still include Bearer token as AuthFilter itself is not mocked
-        ResultActions result = mockMvc.perform(get("/hello")
-                .contentType(MediaType.APPLICATION_JSON)
-                .header("Authorization", "Bearer MockedToken"));
-
-        // Assert
-        result.andExpect(status().isOk());
-
-        String response = result.andReturn().getResponse().getContentAsString();
-
-        assertThat(response).isEqualTo("Hello ExampleUser");
-
-    }
 }
