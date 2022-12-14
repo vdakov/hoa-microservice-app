e5c62b2cac500a663b09315e0baf1a31d7a9781b
Steve Váradi <I.Varadi-1@student.tudelft.nl>
2022-12-15 00:46:23 +0100

    add creation endpoint to hoa controller

--- hoa-microservice/src/main/java/nl/tudelft/sem/template/hoa/controllers/HoaController.java
+++ hoa-microservice/src/main/java/nl/tudelft/sem/template/hoa/controllers/HoaController.java
@@ -2,10 +2,13 @@ package nl.tudelft.sem.template.hoa.controllers;
 
 import java.util.List;
 import nl.tudelft.sem.template.hoa.entitites.Hoa;
+import nl.tudelft.sem.template.hoa.models.HoaModel;
 import nl.tudelft.sem.template.hoa.services.HoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -34,6 +37,13 @@ public class HoaController {
     public ResponseEntity<List<Hoa>> getAllHoas() {
         return ResponseEntity.ok(hoaService.getAllHoas());
     }
+
+    @PostMapping("/createHoa")
+    public ResponseEntity<Hoa> createHoa(@RequestBody HoaModel hoaModel) throws Exception {
+        Hoa hoa = hoaService.createHoa(hoaModel.getName(), hoaModel.getCountry(), hoaModel.getCity());
+        return ResponseEntity.ok(hoa);
+    }
+
 
 
 }
