3b2e1207545e3e0895d4f6184365e2ddd421daf2
Gijs Hoedemaker <g.p.f.hoedemaker@student.tudelft.nl>
2022-12-19 11:02:42 +0100

    Added NoArgsConstructor tags to models

--- commons/src/main/java/nl/tudelft/sem/template/commons/models/CreateReportModel.java
+++ commons/src/main/java/nl/tudelft/sem/template/commons/models/CreateReportModel.java
@@ -1,8 +1,10 @@
 package nl.tudelft.sem.template.commons.models;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class CreateReportModel {
     private String reportBy;
     private String reportedUser;
