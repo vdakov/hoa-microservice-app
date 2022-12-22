7ea18b4f7e451f9579af3d424353794fb25a6fbd
Pál Patrik Varga <VargaPalPatrik@student.tudelft.nl>
2022-12-22 10:42:13 +0100

    Testing activities for user

--- commons/src/main/java/nl/tudelft/sem/template/commons/models/hoa/JoinModel.java
+++ commons/src/main/java/nl/tudelft/sem/template/commons/models/hoa/JoinModel.java
@@ -1,10 +1,19 @@
 package nl.tudelft.sem.template.commons.models.hoa;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class JoinModel extends FullAddressModel {
     private String hoaName;
     private String userDisplayName;
+
+    public JoinModel(String country, String city, String street, String houseNumber,
+                     String postalCode, String hoaName, String userDisplayName){
+        super(country, city, street, houseNumber, postalCode);
+        this.hoaName = hoaName;
+        this.userDisplayName = userDisplayName;
+    }
 }
 
