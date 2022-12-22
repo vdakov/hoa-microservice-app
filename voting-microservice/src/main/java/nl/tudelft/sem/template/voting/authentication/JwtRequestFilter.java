05a825bbf413a1b385795085fa1902970c8630f1
Simeon Atanasov <S.Atanasov@student.tudelft.nl>
2022-12-22 14:44:15 +0100

    Fix the Invalid authorization header message
    This now will get sent only if the header is incorrect

--- voting-microservice/src/main/java/nl/tudelft/sem/template/voting/authentication/JwtRequestFilter.java
+++ voting-microservice/src/main/java/nl/tudelft/sem/template/voting/authentication/JwtRequestFilter.java
@@ -82,8 +82,9 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                 } catch (IllegalArgumentException | JwtException e) {
                     System.err.println("Unable to parse JWT token");
                 }
+            } else {
+                System.err.println("Invalid authorization header");
             }
-            System.err.println("Invalid authorization header");
         }
 
         filterChain.doFilter(request, response);
